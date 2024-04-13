069148fdcb2ed762825f68a97345950ada3e219c
Thomas Christensen <thomas@chrstnsn.dk>
2024-04-13 11:01:50 +0200

    Introduce Spectre and Table rendering

--- StockClientTui/Program.cs
+++ StockClientTui/Program.cs
@@ -1,28 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using Google.Protobuf.WellKnownTypes;
-using Grpc.Core;
+﻿using Grpc.Core;
 using Grpc.Net.Client;
 using StockServer;
+using Spectre.Console;
+using Spectre.Console.Rendering;
 
 using var channel = GrpcChannel.ForAddress("http://localhost:8042");
-var client = new StockServer.Updates.UpdatesClient(channel);
+var client = new Updates.UpdatesClient(channel);
 
-Console.WriteLine("Starting stream from server:");
+var symbols = new[] { "INTC", "AMD", "TSLA", "RIVN", "QTTB",  };
 
-var symbols = new[] { "IBM", "Meta", "Alpha", "Apple", "Tesla" };
+using var call = client.GetStockUpdates(new StockRequest { Symbols = { symbols } });
+var streamOfStocks = call.ResponseStream;
 
-await StreamFromServer(client, symbols);
+var table = new Table().Centered();
+table.Title($"Stock updates");
+table.AddColumn("Symbol");
+table.AddColumn(new TableColumn("Bid").RightAligned());
+table.AddColumn(new TableColumn("Ask").RightAligned());
+table.Border(TableBorder.Ascii);
 
-Console.WriteLine("Stream ended, bye bye...");
-
-
-static async Task StreamFromServer(Updates.UpdatesClient client, string[] symbols)
-{
-    using var call = client.GetStockUpdates(new StockRequest{ Symbols = { symbols }});
-
-    await foreach (var update in call.ResponseStream.ReadAllAsync())
+await AnsiConsole.Live(table)
+    .StartAsync(async ctx =>
     {
-        Console.WriteLine($"{update.Symbol}: Bid: {update.Bid} Ask: {update.Ask}");
+        var state = new State();
+        await foreach (var update in streamOfStocks.ReadAllAsync())
+        {
+            state.UpdateAndRender(update, table);
+            ctx.Refresh();
+        }
+    });
+
+public class State
+{
+    private readonly Dictionary<string, StockReply> _symbols;
+    private int _numberOfUpdates;
+
+    public State()
+    {
+        _symbols = new Dictionary<string, StockReply>();
+        _numberOfUpdates = 0;
+    }
+
+    public void UpdateAndRender(StockReply stock, Table table)
+    {
+        _symbols[stock.Symbol] = stock;
+
+        _numberOfUpdates++;
+
+        table.Rows.Clear();
+
+        foreach (var row in _symbols.Values
+                     .OrderBy(s => s.Symbol)
+                     .Select(stock =>
+                         new TableRow(new IRenderable[]
+                         {
+                             new Markup($"[bold yellow underline]{stock.Symbol}[/]"),
+                             new Markup($"[green]{stock.Bid:0.000}[/]"),
+                             new Markup($"[red]{stock.Ask:0.000}[/]"),
+                         })))
+        {
+            table.Rows.Add(row);
+        }
+        
+        table.Caption($"Updates: {_numberOfUpdates}");
     }
 }
